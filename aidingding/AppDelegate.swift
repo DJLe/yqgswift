c626d10858a5e7d8b7f1bb0c8f842850459bbc6c
xuhao <1872490458@qq.com>
2016-06-29 15:24:17 +0800

    testcommit

--- aidingding/AppDelegate.swift
+++ aidingding/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        // 测试我的 push  加 commit
+        
+        
+        
         return true
     }
 
