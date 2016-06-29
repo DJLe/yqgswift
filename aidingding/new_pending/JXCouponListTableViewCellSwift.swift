9db1b05ac61f2459e85bddb127072bef223b78e7
xuhao <1872490458@qq.com>
2016-06-29 16:18:24 +0800

    jiacell

---
+++ aidingding/new_pending/JXCouponListTableViewCellSwift.swift
@@ -0,0 +1,42 @@
+//
+//  JXCouponListTableViewCell.swift
+//  HongDian
+//
+//  Created by 单于 on 16/1/13.
+//  Copyright © 2016年 BJ HL. All rights reserved.
+//
+
+let JXCouponListTableViewCellSwiftReusedId  = "JXCouponListTableViewCellSwift"
+
+import UIKit
+
+class JXCouponListTableViewCellSwift: JXTableViewCell {
+
+    
+    @IBOutlet weak var couponNameLabel: UILabel!
+    @IBOutlet weak var couponExpireTimesLabel: UILabel!
+    @IBOutlet weak var couponDiscountLabel: UILabel!
+    @IBOutlet weak var couponStatusLabel: UILabel!
+    
+    override func awakeFromNib() {
+       couponNameLabel.font = JXC_FONT_SMALL
+        
+    }
+    
+    // 重写 set 方法
+    var couponModel :JXCouponModel? {
+    
+        didSet{
+            self.couponNameLabel.text = couponModel!.name;
+            
+            let expireTimestamp = (couponModel?.expireTimestamp)! as NSString
+            let expireTimestampSubString = expireTimestamp.substringToIndex(10) as String
+            
+            self.couponExpireTimesLabel.text = expireTimestampSubString
+            
+            self.couponDiscountLabel.text = "现金券" +  "\(couponModel!.discount)"
+            
+        }
+    }
+
+}
